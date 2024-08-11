6e80d90fd0f70ce9fdb21c705fa4fac45684aa8f
Andrew Marcuse <fileformat@gmail.com>
2024-08-10 20:24:20 -0400

    ADD: hello world

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# run locally for dev
+#
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+ENV_FILE="${1:-./.env}"
+if [ -f "${ENV_FILE}" ]; then
+    echo "INFO: loading '${ENV_FILE}'!"
+    export $(cat "${ENV_FILE}")
+fi
+
+
+# run the app
+cargo watch -x run
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+
+    // get address from environment variable
+    let address = std::env::var("ADDRESS").unwrap_or_else(|_| "0.0.0.0".to_string());
+
+    // get port from environment variable
+    let port = std::env::var("PORT").unwrap_or_else(|_| "4000".to_string());
+
+    let listen = format!("{}:{}", address, port);
+
+    let listener = tokio::net::TcpListener::bind(listen).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
